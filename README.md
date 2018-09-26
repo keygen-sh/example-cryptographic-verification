abdebdedc9e28754eea23dc8b59aebaf5af7e3a5
ezekg <ezekg@yahoo.com>
2018-09-26 00:15:31 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Example Cryptographic Verification
+This is an example of cryptographically validating license keys using
+your Keygen account's public key. You can find your public key within
+[your account's settings page](https://app.keygen.sh/settings).
+
+Cryptographically validating schemed licenses can be used to implement
+offline licensing, as well as adding additional security to your
+licensing model. All that is needed to cryptographically validate
+a license is your account's public key.
+
+The license's policy _must_ implement one of the following [schemes](https://keygen.sh/docs/api/#policies-create-attrs-scheme):
+
+- `RSA_2048_PKCS1_ENCRYPT`
+- `RSA_2048_PKCS1_SIGN`
+- `RSA_2048_PKCS1_PSS_SIGN`
+- `RSA_2048_JWT_RS256`
+
+## Running the example
+
+First up, add an environment variable containing your public key:
+```bash
+# Your Keygen account's public key (make sure it is *exact* - newlines and all)
+export KEYGEN_PUBLIC_KEY=$(printf %b \
+  '-----BEGIN PUBLIC KEY-----\n' \
+  'zdL8BgMFM7p7+FGEGuH1I0KBaMcB/RZZSUu4yTBMu0pJw2EWzr3CrOOiXQI3+6bA\n' \
+  # …
+  'efK41Ml6OwZB3tchqGmpuAsCEwEAaQ==\n' \
+  '-----END PUBLIC KEY-----')
+```
+
+You can either run each line above within your terminal session before
+starting the app, or you can add the above contents to your `~/.bashrc`
+file and then run `source ~/.bashrc` after saving the file.
+
+Next, install dependencies with [`yarn`](https://yarnpkg.comg):
+```
+yarn
+```
+
+Then run the script, passing in the `key` as well as the `scheme`:
+```
+yarn start --scheme RSA_2048_PKCS1_PSS_SIGN --key SOME_LICENSE_KEY_HERE
+yarn start -s RSA_2048_PKCS1_PSS_SIGN -k SOME_LICENSE_KEY_HERE
+```
+
+How a given license key is validated will depend on the scheme. Please
+review the code for a more thorough overview of how each scheme is
+validated. Be sure to copy your public key correctly - your keys will
+fail validation if it is copied incorrectly. You can find your public
+key in [your account's settings](https://app.keygen.sh/settings).
+
+## Questions?
+
+Reach out at [support@keygen.sh](mailto:support@keygen.sh) if you have any
+questions or concerns!
---
+++ index.js
@@ -0,0 +1,133 @@
+const { KEYGEN_PUBLIC_KEY } = process.env
+
+const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
+const chalk = require('chalk')
+
+async function main() {
+  let scheme, key
+
+  // Parse argument flags
+  process.argv.forEach((arg, i, argv) => {
+    switch (arg) {
+      case '--scheme':
+      case '-s':
+        scheme = argv[i + 1]
+        break
+      case '--key':
+      case '-k':
+        key = argv[i + 1]
+        break
+    }
+  })
+
+  // Sanity checks for public key
+  if (!KEYGEN_PUBLIC_KEY) {
+    throw new Error('Public key is required')
+  }
+
+  if (!KEYGEN_PUBLIC_KEY.includes(`-----BEGIN PUBLIC KEY-----`) ||
+      !KEYGEN_PUBLIC_KEY.includes(`-----END PUBLIC KEY-----`)) {
+    throw new Error('Public key is not valid')
+  }
+
+  // Validate flags
+  if (!scheme) {
+    throw new Error('Scheme is required')
+  }
+
+  if (!key) {
+    throw new Error('Key is required')
+  }
+
+  switch (scheme) {
+    // Decrypt license key that is encrypted using RSA's PKCS1 v1.5 padding
+    case 'RSA_2048_PKCS1_ENCRYPT': {
+      // Decode the base64 encoded key
+      const buf = Buffer.from(key, 'base64')
+
+      // Decrypt the key
+      try {
+        const decryptedKey = crypto.publicDecrypt({ key: KEYGEN_PUBLIC_KEY, padding: crypto.constants.RSA_PKCS1_PADDING }, buf)
+
+        console.log(chalk.green(`License key is cryptographically valid!`))
+        console.log(chalk.gray(`Decrypted: ${decryptedKey}`))
+      } catch (e) {
+        console.error(chalk.red('License key is not valid!'))
+      }
+
+      break
+    }
+    // Verify license key that is signed using RSA's PKCS1 v1.5 padding
+    case 'RSA_2048_PKCS1_SIGN': {
+      // Extract key and signature from the license key string
+      const [enc, sig] = key.split('.')
+
+      // Decode the base64 encoded key
+      const dec = Buffer.from(enc, 'base64').toString()
+
+      // Verify the signature
+      const verifier = crypto.createVerify('sha256')
+      verifier.write(dec)
+      verifier.end()
+
+      const ok = verifier.verify({ key: KEYGEN_PUBLIC_KEY, padding: crypto.constants.RSA_PKCS1_PADDING }, sig, 'base64')
+      if (ok) {
+        console.log(chalk.green(`License key is cryptographically valid!`))
+        console.log(chalk.gray(`Decoded: ${dec}`))
+      } else {
+        console.error(chalk.red('License key is not valid!'))
+      }
+
+      break
+    }
+    // Verify license key that is signed using RSA's PKCS1-PSS padding
+    case 'RSA_2048_PKCS1_PSS_SIGN': {
+      // Extract key and signature from the license key string
+      const [enc, sig] = key.split('.')
+
+      // Decode the base64 encoded key
+      const dec = Buffer.from(enc, 'base64').toString()
+
+      // Verify the signature
+      const verifier = crypto.createVerify('sha256')
+      verifier.write(dec)
+      verifier.end()
+
+      const ok = verifier.verify({ key: KEYGEN_PUBLIC_KEY, padding: crypto.constants.RSA_PKCS1_PSS_PADDING }, sig, 'base64')
+      if (ok) {
+        console.log(chalk.green(`License key is cryptographically valid!`))
+        console.log(chalk.gray(`Decoded: ${dec}`))
+      } else {
+        console.error(chalk.red('License key is not valid!'))
+      }
+
+      break
+    }
+    // Verify a JWT license key that is signed using the RS256 algo
+    case 'RSA_2048_JWT_RS256': {
+      try {
+        const dec = jwt.verify(key, KEYGEN_PUBLIC_KEY, { algorithms: ['RS256'] })
+
+        console.log(chalk.green(`License key is cryptographically valid!`))
+        console.log(
+          chalk.gray(`Claims: ${JSON.stringify(dec, null, 2)}`)
+        )
+      } catch (e) {
+        console.error(
+          chalk.red(`License key is not valid (${e.message})`)
+        )
+      }
+
+      break
+    }
+    default:
+      throw new Error(`Unsupported scheme ${scheme}`)
+  }
+}
+
+main().catch(err =>
+  console.error(
+    chalk.red(`Error: ${err.message}`)
+  )
+)
